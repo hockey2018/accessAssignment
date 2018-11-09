e44770fc1a2e9e7d6d10088b37a8d7b60b25d9f4
hockey2018 <sulliva2@tcd.ie>
2018-11-09 12:10:40 +0000

    commit 3

--- accessAss.R
+++ accessAss.R
@@ -9,3 +9,11 @@ library(jsonlite)
 library(httpuv)
 #install.packages("httr")
 library(httr)
+
+# this can be any application but here we will use github
+oauth_endpoints("github")
+
+# this refers to the oauth that I personally generated
+myapp <- oauth_app(appname = "aoifesAccessAssignment",
+                   key = "a18829be5dfb86ca6f94",
+                   secret = "c1a561fa6b32bbd3b8dbb77a8f45df83f906021e")
